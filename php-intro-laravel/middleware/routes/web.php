0ccc995899a97012204e09845dfd42935abe26d1
Jose Angel Garcia <jagspage2012@gmail.com>
2021-01-28 23:03:46 -0600

    middleware

---
+++ php-intro-laravel/middleware/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('auth'); // agrega autenticación a este endpoint
+
+
+Route::resource('user', 'UserController')->middleware('auth'); // agregar middleware atodos los endpoints )7  metodos http) 
